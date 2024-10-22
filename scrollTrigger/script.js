551891c34534d0c30d212dae1dc8df6ac4965284
atharvabhang169@gmail.com <atharvabhang169@gmail.com>
2024-10-22 07:35:55 +0530

    scroll trigger end

--- scrollTrigger/script.js
+++ scrollTrigger/script.js
@@ -1,19 +1,77 @@
-gsap.from("#page1 #box",{
-    scale:0,
-    delay:1,
-    duration:1,
-    rotate:360
-})
+// gsap.from("#page1 #box",{
+//     scale:0,
+//     delay:1,
+//     duration:1,
+//     rotate:360
+// })
 
-gsap.from("#page2 #box",{
-    scale:0,
-    delay:1,
-    duration:1,
-    rotate:360,
+// gsap.from("#page2 #box",{
+//     scale:0,
+//     duration:1,
+//     rotate:360,
+//     scrollTrigger:{
+//         trigger:"#page2 #box",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60%"
+//     }
+// })
+
+// ******************Text ka design***********************
+// gsap.from("#page2 h1",{
+//     opacity:0,
+//     duration:2,
+//     x:500,
+//     scrollTrigger:{
+//         trigger:"#page2 h1",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 50%"
+//     }
+// })
+
+// gsap.from("#page2 h2",{
+//     opacity:0,
+//     duration:2,
+//     x:-500,
+//     scrollTrigger:{
+//         trigger:"#page2 h2",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 50%"
+//     }
+// })
+
+
+
+// gsap.from("#page2 #box",{
+//     scale:0,
+//     opacity:0,
+//     duration:1,
+//     rotate:720,
+//     scrollTrigger:{
+//         trigger:"#page2 #box",
+//         scroller:"body",
+//         markers:true,
+//         staart:"top 60%",
+//         end:"top 30%",
+//         //scrub:true
+//         scrub:2
+//     }
+// })
+
+// ******************lecture  2 ka Text wala*********
+
+// Rule1-when use "pin" property trigger the parent
+gsap.to("#page2 h1",{
+    transform:"translateX(-150%)",
     scrollTrigger:{
-        trigger:"#page2 #box",
+        trigger:"#page2",
         scroller:"body",
         markers:true,
-        start:"top 60%"
+        start:"top 0%",
+        end:"top -100%",
+        scrub:2,
+        pin:true
     }
 })
